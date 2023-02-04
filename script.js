9533b69ab2d6e9c62c522db7b96a3288afefeadf
Konstantin <example@gmail.com>
2023-02-04 18:46:38 +0300

    first step

---
+++ script.js
@@ -0,0 +1,34 @@
+// let answer = prompt('Сколько фильмов вы уже посмотрели?');
+// let numberOfFilms = answer;
+
+// let personalMovieDB =  {
+//     count: answer,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// }
+
+// prompt('Один из последних просмотренных фильмов?', '') = oneOfLastFilms;
+
+// personalMovieDB.movies.oneOfLastFilms;
+// console.log(personalMovieDB)
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');  
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
